aaa83b8e7a4f864bc77c223cb4357ea5ecdee40d
khaoulabelahsen <khbelahsensa@gmail.com>
2021-02-04 19:04:58 +0100

    submission

--- du_submission/scripts/processing.py
+++ du_submission/scripts/processing.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import pandas as pd 
+from collections import Counter 
+import numpy as np 
 
 def full_processing(dataset):
     """Return the full processed version of any dataset with the same features as the train set 
@@ -25,11 +27,17 @@ def money_processing(dataset, features):
 def binary_processing(dataset):
     """Function to process specific features in the dataset 
     """
-    dataset['MSTATUS'] = dataset['MSTATUS'].apply(lambda x : 1 if x =='Yes' else 0)
-    dataset['SEX'] = dataset['SEX'].apply(lambda x : 1 if x =='M' else 0)
-    dataset['PARENT1'] = dataset['PARENT1'].apply(lambda x : 1 if x =='Yes' else 0)
-    dataset['RED_CAR'] = dataset['RED_CAR'].apply(lambda x : 1 if x =='yes' else 0)
-    dataset['REVOKED'] = dataset['REVOKED'].apply(lambda x : 1 if x =='Yes' else 0)
+    # dataset['MSTATUS'] = dataset['MSTATUS'].apply(lambda x : 1 if x =='Yes' else 0)
+    # dataset['SEX'] = dataset['SEX'].apply(lambda x : 1 if x =='M' else 0)
+    # dataset['PARENT1'] = dataset['PARENT1'].apply(lambda x : 1 if x =='Yes' else 0)
+    # dataset['RED_CAR'] = dataset['RED_CAR'].apply(lambda x : 1 if x =='yes' else 0)
+    # dataset['REVOKED'] = dataset['REVOKED'].apply(lambda x : 1 if x =='Yes' else 0)
+
+    dataset['MSTATUS'] = dataset['MSTATUS'].astype('category')
+    dataset['SEX'] = dataset['SEX'].astype('category')
+    dataset['PARENT1'] = dataset['PARENT1'].astype('category')
+    dataset['RED_CAR'] = dataset['RED_CAR'].astype('category')
+    dataset['REVOKED'] = dataset['REVOKED'].astype('category')
 
     return 0 
 
@@ -64,3 +72,21 @@ def replace_with_method(dataset, features, method):
             raise NotImplementedError
 
     return 0 
+
+def detect_outlier(df):
+    features = df.columns
+    outliers  = []
+    for i, feature in enumerate(features):
+        if df[feature].dtype == 'float64':
+            # Calculate Q1 (25th percentile of the data) for the given feature
+            Q1 = np.percentile(df[feature], 25)
+            # Calculate Q3 (75th percentile of the data) for the given feature
+            Q3 = np.percentile(df[feature], 75)
+            # Use the interquartile range to calculate an outlier step
+            step = 1.5 * (Q3 - Q1)
+            feature_outliers = df[~((df[feature] >= Q1 - step) & (df[feature] <= Q3 + step))]
+            outliers.extend(list(feature_outliers.index.values))
+            print('Feature: {}, outliers: {}\n'.format(feature, len(feature_outliers.index)))
+    
+    features_outliers = (Counter(outliers) - Counter(set(outliers))).keys()
+    return features_outliers
